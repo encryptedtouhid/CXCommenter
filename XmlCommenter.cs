5230a223585080de6909a7533dd1ea8ae55485ce
Khaled Md Tuhidul Hossain <me@tuhidulhossain.com>
2023-03-23 00:00:27 +0800

    Update XmlCommenter.cs

--- XmlCommenter.cs
+++ XmlCommenter.cs
@@ -1,4 +1,5 @@
-﻿using EnvDTE;
+﻿using Community.VisualStudio.Toolkit;
+using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
 using System;
@@ -14,16 +15,10 @@ namespace CXCommenter
     {
         public static void CommentSolution()
         {
-            // Get the DTE object.
             DTE2 dte = ServiceProvider.GlobalProvider.GetService(typeof(DTE)) as DTE2;
-
-            // Get the solution object.
             Solution2 solution = dte.Solution as Solution2;
-
-            // Loop through all projects in the solution.
             foreach (Project project in solution.Projects)
             {
-                // Add XML comments to the project.
                 CommentProject(project);
             }
         }
@@ -66,9 +61,9 @@ namespace CXCommenter
                                     string comment = "/// <summary>" + Environment.NewLine +
                                                   "/// TODO: Add XML comment for " + functionName + Environment.NewLine +
                                                   "/// </summary>" + Environment.NewLine +
-                                                  "///<param>" + parameters[0]  + "</param>" + Environment.NewLine +
-                                                  "///<return>" + returnType + "</return>" + Environment.NewLine ;
-                                               
+                                                  "///<param>" + parameters[0] + "</param>" + Environment.NewLine +
+                                                  "///<return>" + returnType + "</return>" + Environment.NewLine;
+
                                     codeElement2.StartPoint.CreateEditPoint().Insert(comment);
 
                                     // Format the XML comment
@@ -79,7 +74,7 @@ namespace CXCommenter
 
 
                                 }
-                                else if(memberElement.Kind == vsCMElement.vsCMElementProperty)
+                                else if (memberElement.Kind == vsCMElement.vsCMElementProperty)
                                 {
                                     string comment = "/// <summary>" + Environment.NewLine +
                                                    "/// TODO: Add XML comment for " + codeElement2.Name + Environment.NewLine +
@@ -98,6 +93,81 @@ namespace CXCommenter
                 }
             }
 
+            foreach (ProjectItem item in project.ProjectItems)
+            {
+                if (item.SubProject != null)
+                {
+                    IterateThroughAllCodeElements(item.SubProject);
+                }
+                //else if (item.ProjectItems != null && item.ProjectItems.Count > 0)
+                //{
+                //    // This is a folder containing other items
+                //    IterateThroughAllCodeElementsInFolder(item);
+                //}
+            }
+
         }
+
+        public static void IterateThroughAllCodeElements(Project SubProject)
+        {
+
+            foreach (CodeElement codeElement in SubProject.CodeModel.CodeElements)
+            {
+                if (codeElement.Kind == vsCMElement.vsCMElementNamespace)
+                {
+                    CodeNamespace codeNamespace = (CodeNamespace)codeElement;
+
+                    foreach (CodeElement typeElement in codeNamespace.Members)
+                    {
+                        if (typeElement.Kind == vsCMElement.vsCMElementClass ||
+                            typeElement.Kind == vsCMElement.vsCMElementStruct ||
+                            typeElement.Kind == vsCMElement.vsCMElementInterface)
+                        {
+                            CodeType codeType = (CodeType)typeElement;
+
+                            foreach (CodeElement memberElement in codeType.Members)
+                            {
+                                CodeElement2 codeElement2 = (CodeElement2)memberElement;
+
+                                if (memberElement.Kind == vsCMElement.vsCMElementFunction)
+                                {
+                                    CodeFunction codeFunction = (CodeFunction)memberElement;
+                                    string functionName = codeFunction.Name;
+                                    string returnType = codeFunction.Type.AsString;
+                                    List<string> parameters = new List<string>();
+                                    foreach (CodeParameter parameter in codeFunction.Parameters)
+                                    {
+                                        parameters.Add(parameter.Type.AsString + " " + parameter.Name);
+                                    }
+
+                                    string comment = "/// <summary>" + Environment.NewLine +
+                                                  "/// TODO: Add XML comment for " + functionName + Environment.NewLine +
+                                                  "/// </summary>" + Environment.NewLine +
+                                                  "///<param>" + parameters[0] + "</param>" + Environment.NewLine +
+                                                  "///<return>" + returnType + "</return>" + Environment.NewLine;
+
+                                    codeElement2.StartPoint.CreateEditPoint().Insert(comment);
+                                    codeElement2.StartPoint.CreateEditPoint().SmartFormat(codeElement2.EndPoint);
+                                    codeElement2.ProjectItem.Save();
+
+
+                                }
+                                else if (memberElement.Kind == vsCMElement.vsCMElementProperty)
+                                {
+                                    string comment = "/// <summary>" + Environment.NewLine +
+                                                   "/// TODO: Add XML comment for " + codeElement2.Name + Environment.NewLine +
+                                                   "/// </summary>" + Environment.NewLine;
+                                    codeElement2.StartPoint.CreateEditPoint().Insert(comment);
+                                    codeElement2.StartPoint.CreateEditPoint().SmartFormat(codeElement2.EndPoint);
+                                    codeElement2.ProjectItem.Save();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+       
     }
 }

db8e68cabb1ee7e71c8e62086bb933282cc3b210
Khaled Md Tuhidul Hossain <me@tuhidulhossain.com>
2023-03-23 22:23:04 +0800

    Updated Company name

--- source.extension.cs
+++ source.extension.cs
@@ -8,11 +8,11 @@ namespace CXCommenter
     internal sealed partial class Vsix
     {
         public const string Id = "CXCommenter.55051f0e-32a9-4a22-932f-1602de247f6b";
-        public const string Name = "CXCommenter";
+        public const string Name = "CXCommenter (Preview)";
         public const string Description = @"An  automatic Comment Generating Tool For Visual Studio.";
         public const string Language = "en-US";
-        public const string Version = "1.0";
-        public const string Author = "Company";
+        public const string Version = "0.5";
+        public const string Author = "Khaled Md Tuhidul Hossain";
         public const string Tags = "XML-commenter, Documentation-Generator, XML-Comments,Productivity-Enhancement,Code-Insights";
     }
 }

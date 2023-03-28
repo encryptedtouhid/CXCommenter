8c1f3f1e2f02a97c2fec5c92db6bf8ba33c09f31
Khaled Md Tuhidul Hossain <me@tuhidulhossain.com>
2023-03-28 17:54:52 +0800

    Update ImportKeyFile.ps1

--- ImportKeyFile.ps1
+++ ImportKeyFile.ps1
@@ -4,16 +4,15 @@ Param(
     [string]$keyFilePassword
 )
 
-$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList @($keyFilePath, $keyFilePassword, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::PersistKeySet)
+$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+$cert.Import($keyFilePath, $keyFilePassword, [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::PersistKeySet)
 
 $cspParams = New-Object System.Security.Cryptography.CspParameters
 $cspParams.KeyContainerName = $keyContainerName
 $cspParams.Flags = [System.Security.Cryptography.CspProviderFlags]::UseMachineKeyStore
 
-$rsaProvider = [System.Security.Cryptography.RSACryptoServiceProvider]::new($cspParams)
-
-$rsaCert = $cert.PublicKey.Key.CopyWithPrivateKey($cert.PrivateKey)
-$rsaProvider.ImportParameters($rsaCert.ExportParameters($true))
+$rsaProvider = New-Object System.Security.Cryptography.RSACryptoServiceProvider
+$rsaProvider.ImportParameters($cert.PrivateKey.ExportParameters($true))
 $rsaProvider.PersistKeyInCsp = $true
 
 $null = $rsaProvider.ToXmlString($true)

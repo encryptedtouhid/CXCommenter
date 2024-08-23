f132f7cd1f3ecfd2467a861f3adbd817e06bf074
Khaled <me@tuhidulhossain.com>
2024-08-23 22:10:01 +0800

    Structure Changed For Comment.

---
+++ Models/Parameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CXCommenter.Models
+{
+    public class Parameters
+    {
+        public string VariableName { get; set; }
+        public string TypeName { get; set; }
+    }
+}
